10e69c8a8c05b6c3a81e6692a6c2e62755ae232d
Zobris <zobris@gmail.com>
2020-11-13 11:32:02 -0500

    update
    added doc

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # VEL
 Taunts for Smite
 ## Notes
-Works only for starting Smite from Steam. Steam must be installed to default
-`C:\Program Files (x86)`
-***
+Works only for starting Smite from Steam. Steam must be installed to default `C:\Program Files (x86)`
 ## About
 - __Version__: 1.0
 - __Release Date__: 11/12/2020
@@ -12,7 +10,6 @@ Works only for starting Smite from Steam. Steam must be installed to default
 - __Download__:
     - __[VEL.py](https://raw.githubusercontent.com/cyoung502/VEL/main/VEL.py)__
     - __[VEL.exe](https://github.com/cyoung502/VEL/raw/main/dist/VEL.exe)__ [Compiled with _pyinstaller 3.4_]
-***
 ## Setup
 ### Running from .exe
 Simply start VEL.exe. The program will auto start Smite, and will run if you already have Smite started.
@@ -24,12 +21,10 @@ In order to run from python you must have the following packages installed:
 CMD to the directory and run:
 
     python VEL.py
-***
 ## Controls
-    [ F1 ] --> Quit
-    [ ` ] --> Joke
-    [ CAPS ] --> Taunt
-    [ LSHIFT ] --> Joke
-***
+    [ F1 ]     ---> Quit
+    [ ` ]      ---> Joke
+    [ CAPS ]   ---> Taunt
+    [ LSHIFT ] ---> Joke
 ## Credits
 - [BleuBear](https://www.deviantart.com/bleubear) - Ra Image
--- VEL.py
+++ VEL.py
@@ -5,9 +5,16 @@ from pynput import keyboard
 import sys
 
 def startSmite():
+    """Starts Smite from Steam by using Steam app ID (386360).
+    """
     subprocess.call(r"C:\Program Files (x86)\Steam\Steam.exe -applaunch 386360")
 
 def smiteRunning():
+    """Checks to see if Smite is currently running.
+
+    Returns:
+        bool: The return value. Returns True if Smite is running, False otherwise.
+    """
     for process in psutil.process_iter():
         try:
             if "smite" in process.name().lower():
@@ -16,35 +23,69 @@ def smiteRunning():
             pass
     return False
 
+# kb is our keyboard object
 kb = keyboard.Controller()
 def keyDown(key, delay=0.001):
+    """Presses a key down.
+
+    Args:
+        key (int): The keycode of the key to press.
+        delay (float, optional): Time to sleep after key press. Defaults to 0.001 (1ms).
+    """
     kb.type(key)
     sleep(delay)
 
 def keyUp(key, delay=0.001):
+    """Releases a key that is pressed.
+
+    Args:
+        key (int): The keycode of the key to press.
+        delay (float, optional): Time to sleep after key release. Defaults to 0.001 (1ms).
+    """
     kb.release(key)
     sleep(delay)
 
 def type(key, delay=0.001):
+    """Simulates a single keystroke, pressing a key and releasing it.
+
+    Args:
+        key (int): The keycode of the key to press.
+        delay (float, optional): Time to sleep after press and release. Defaults to 0.001 (1ms).
+    """
     keyDown(key, delay)
     keyUp(key, delay)
 
 def tauntLaugh():
+    """Types the keys [VEL] for laugh taunt.
+    """
     type('v')
     type('e')
     type('l')
 
 def tauntTaunt():
+    """Types the keys [VET] for taunt taunt.
+    """
     type('v')
     type('e')
     type('t')
 
 def tauntJoke():
+    """Types the keys [VEJ] for joke taunt.
+    """
     type('v')
     type('e')
-    type('l')
+    type('j')
 
-def on_type(key):
+def on_press(key):
+    """Callback function for our keyboard listener. Listens for a key and will carry out the following if pressed:
+        - Left Shift: Laugh
+        - Caps Lock: Taunt
+        - Tilde: Joke
+        - F1: Quit
+
+    Args:
+        key (int): The keycode of the key press to listen for.
+    """
     if key == keyboard.Key.shift_l:
         tauntLaugh()
     elif key == keyboard.Key.caps_lock:
@@ -52,14 +93,19 @@ def on_type(key):
     elif key == keyboard.KeyCode.from_char('`'):
         tauntJoke()
     elif key == keyboard.Key.f1:
+        keyboard.Listener.stop()
         sys.exit()
         quit()
 
 def setupListener():
+    """Setups keyboard listener in separate thread.
+    """
     with keyboard.Listener(on_press=on_press) as listener:
         listener.join()
 
 def main():
+    """Main Loop. Will start Smite if not launched. If Smite is already launched, keyboard listener will start.
+    """
     print("VEL Smite Taunts Started!")
     if not smiteRunning():
         startSmite()
@@ -69,10 +115,10 @@ def main():
         print("Smite Started!")
 
     print("===== Controls =====")
-    print("[ F1 ] --> Quit")
-    print("[ ` ] --> Joke")
-    print("[ CAPS ] --> Taunt")
-    print("[ LSHIFT ] --> Joke")
+    print("[ F1 ]     ----> Quit")
+    print("[ ` ]      ----> Joke")
+    print("[ CAPS ]   ----> Taunt")
+    print("[ LSHIFT ] ----> Joke")
     setupListener()
 
 main()
