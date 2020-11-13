24067ad417ff168867c9e625797847c22dd866d8
Zobris <zobris@gmail.com>
2020-11-12 21:21:28 -0500

    1st

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# smite-taunts
- Taunts for Smite
+# VEL
+Taunts for Smite
+## About
+- __Version__: 1.0
+- __Release Date__: 11/12/2020
+- __Platform__: Windows
+[Download](https://google.com)
+## Controls
+    [ F1 ] --> quit
+    [ ` ] --> Joke
+    [ CAPS ] --> Taunt
+    [ LSHIFT ] --> Joke
---
+++ main.py
@@ -0,0 +1,78 @@
+import psutil
+import time
+import sys
+from pynput import keyboard
+
+def processRunning(processName):
+    for process in psutil.process_iter():
+        try:
+            if processName.lower() in process.name().lower():
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            pass
+    return False
+
+
+kb = keyboard.Controller()
+def pressKey(key, delay=0.001):
+    kb.press(key)
+    time.sleep(delay)
+
+def releaseKey(key, delay=0.001):
+    kb.release(key)
+    time.sleep(delay)
+
+def typeKey(key, delay=0.001):
+    pressKey(key, delay)
+    releaseKey(key, delay)
+
+def tauntLaugh():
+    typeKey('v')
+    typeKey('e')
+    typeKey('l')
+
+def tauntTaunt():
+    typeKey('v')
+    typeKey('e')
+    typeKey('t')
+
+def tauntJoke():
+    typeKey('v')
+    typeKey('e')
+    typeKey('l')
+
+def on_press(key):
+    if key == keyboard.Key.shift_l:
+        tauntLaugh()
+    elif key == keyboard.Key.caps_lock:
+        tauntTaunt()
+    elif key == keyboard.KeyCode.from_char('`'):
+        tauntJoke()
+    elif key == keyboard.Key.f1:
+        sys.exit()
+        quit()
+
+def setupListener():
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
+
+def loop():
+    killLoop = False
+    print("Looking for Smite...")
+    while 1:
+        if processRunning("smite"):
+            print("=====Smite Found!=====")
+            print("[ F1 ] --> Quit")
+            print("[ ` ] --> Joke")
+            print("[ CAPS ] --> Taunt")
+            print("[ LSHIFT ] --> Joke")
+            setupListener()
+            killLoop = True
+
+        if killLoop == True:
+            break
+
+        time.sleep(1)
+
+print("Starting Smite Taunts")
+loop()
